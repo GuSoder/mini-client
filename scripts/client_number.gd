3a4eecc2fa009bcd652de956f7528d7995c10cc6
gus <gusoder@gmail.com>
2025-08-27 21:57:04 +0200

    Add publish/subscribe functionality to networked objects
    - Add class_name to ClientNumber for type safety
    - Implement role determination based on client number matching
    - Add timer-based publisher/subscriber role checking every 10ms
    - Create placeholder publish() and subscribe() functions

--- scripts/client_number.gd
+++ scripts/client_number.gd
@@ -1,3 +1,4 @@
+class_name ClientNumber
 extends Node3D
 
 @export var client_number: int = 1
--- scripts/networked_object.gd
+++ scripts/networked_object.gd
@@ -1,1 +1,28 @@
 extends Node3D
+
+@export var client_number: int = 1
+
+var is_publisher: bool = false
+
+func _ready():
+	var timer = Timer.new()
+	timer.wait_time = 0.01
+	timer.timeout.connect(_check_role)
+	timer.autostart = true
+	add_child(timer)
+
+func _check_role():
+	var client_number_node: ClientNumber = get_node("../../ClientNumber")
+	if client_number_node:
+		is_publisher = (client_number == client_number_node.client_number)
+		
+	if is_publisher:
+		publish()
+	else:
+		subscribe()
+
+func publish():
+	pass
+
+func subscribe():
+	pass
